17874979e20fbab8faa646d78ac57c2746f64096
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-14 00:35:14 +0700

    created Role Table

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,23 @@
-CREATE TABLE department (
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+-- CREATE TABLE employees (
+--  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--  first_name VARCHAR(30) NOT NULL,
+--  last_name VARCHAR(30) NOT NULL,
+--  role_id INTEGER,
+--  manager_id INTEGER
+-- );
+
+CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name  VARCHAR(30) NOT NULL
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER
+);
+
+CREATE TABLE department(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,6 +4,27 @@ VALUES
   ('Human Resources'),
   ('Marketing'),
   ('Accounting'),
-  ('Sales' ),
-  ('Advertisement');
+  ('Sales' );
+  
  
+ INSERT INTO role (title,salary,department_id)
+ VALUES
+  ('Full Stack Developer',99000,1),
+  ('Front-End Dev',70000,1),
+  ('Back-End Dev',75000,1),
+  ('IT help',50000,1),
+  ('Recruiter',65000,2),
+  ('HR information specialist',60000,2),
+  ('Digital Marketer',72000,3),
+  ('Content marketing specialist',70000,3),
+  ('Creative Director',100000,3),
+  ('Accoutant',80000,4),
+  ('CPA',88000,4),
+  ('Business Analyst',100000,4),
+  ('Sales consultant',60000,5),
+  ('Sales representative',65000,5),
+  ('Sales manager',90000,5);
+  
+  
+  
+  
