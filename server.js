f1edd9055b8cf40cb458dcb45e5f280d46396e17
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-13 23:05:59 +0700

    View and Create Department

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,14 +7,37 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '01697123060',
+      database: 'company'
+    },
+    console.log('Connected to the company database.')
+  );
+//GET all the department field
+  db.query(`SELECT * FROM department`, (err, rows) => {
+    console.log(rows);
   });
 
-  // Default response for any other request (Not Found)
+  // Create a department
+const sql = `INSERT INTO department (name) 
+VALUES (?)`;
+const params = ['Carleton'];
+
+db.query(sql, params, (err, result) => {
+if (err) {
+console.log(err);
+}
+console.log(result);
+});
+  
+
+  // Default response for any other request (Not Found)-catchall
 app.use((req, res) => {
     res.status(404).end();
   });
