0f827bc6cd69e28f49258a813e31212c42e9fc6a
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-18 23:20:18 +0700

    test constrain role and department tables

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INTEGER
+  department_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
   
 );
 
